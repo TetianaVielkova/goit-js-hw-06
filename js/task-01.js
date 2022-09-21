7b108d5e6ce85dcac61578cd09dc4c485ecaeacb
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2022-09-21 18:43:49 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+
+//Посчитает и выведет в консоль количество категорий в 
+//ul#categories, то есть элементов li.item.
+
+const categoriesEl = document.querySelectorAll('.item');
+console.log(`Number of categories:`, categoriesEl.length);
+
+//Для каждого элемента li.item в списке ul#categories, найдет и выведет в 
+//консоль текст заголовка элемента (тега <h2>) и количество элементов в 
+//категории (всех вложенных в него <li>).
+
+categoriesEl.forEach((element) => {
+    const titleItemEl = element.querySelector('h2').textContent;
+    const firstElementUnderThisName = element.querySelectorAll('li');
+
+    console.log('Category:', titleItemEl);
+    console.log('Elements:', firstElementUnderThisName.length);
+    });
